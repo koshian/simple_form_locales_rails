d571482c698ce15e31c0f8b2a20fa5d259228f23
Sugano Yoshihisa(E) <koshian@foxking.org>
2016-01-24 18:41:21 +0900

    Using Depot::Application.configure

--- lib/simple_form_locales_rails/i18n_ext.rb
+++ lib/simple_form_locales_rails/i18n_ext.rb
@@ -1,3 +1,6 @@
 SIMPLE_FORM_LOCALES_PATH = '../../simple_form_locales/locales'
-config.i18n.load_path +=
-  Pathname.new(File.dirname(__FILE__)).join(SIMPLE_FORM_LOCALES_PATH)
+Depot::Application.configure do
+  config.i18n.load_path +=
+    Pathname.new(File.dirname(__FILE__)).join(SIMPLE_FORM_LOCALES_PATH)
+end
+
