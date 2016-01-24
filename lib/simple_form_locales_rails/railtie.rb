8ddfae43e778b3c15dbda00a4b819d69073b6ec4
Sugano Yoshihisa(E) <koshian@foxking.org>
2016-01-24 19:19:38 +0900

    ('A`)

--- lib/simple_form_locales_rails/railtie.rb
+++ lib/simple_form_locales_rails/railtie.rb
@@ -2,10 +2,10 @@ require 'rails'
 
 module SimpleFormLocalesRails
   class Railtie < ::Rails::Railtie #:nodoc:
-    locale_path = '../../simple_form_locales/locales'
+    locale_path = '../../simple_form_locales/locales/*.yml'
     SimpleFormLocalesRails::Railtie.instance_eval do
-      dirname = Pathname.new(File.dirname(__FILE__)).join(locale_path)
-      I18n.load_path.push dirname
+      files = Dir[File.join(File.dirname(__FILE__), locale_path)]
+      I18n.load_path.push files
     end
   end
 end
