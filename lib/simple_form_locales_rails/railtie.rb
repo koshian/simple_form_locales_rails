82bd7eeacb7035fdfc5f8bf807d5c95723243950
Sugano Yoshihisa(E) <koshian@foxking.org>
2016-01-24 19:08:25 +0900

    ('A`)

--- lib/simple_form_locales_rails/railtie.rb
+++ lib/simple_form_locales_rails/railtie.rb
@@ -1,6 +1,11 @@
-SIMPLE_FORM_LOCALES_PATH = '../../simple_form_locales/locales'
-Depot::Application.configure do
-  config.i18n.load_path +=
-    Pathname.new(File.dirname(__FILE__)).join(SIMPLE_FORM_LOCALES_PATH)
-end
+require 'rails'
 
+module SimpleFormLocalesRails
+  class Railtie < ::Rails::Railtie #:nodoc:
+    locale_path = '../../simple_form_locales/locales'
+    SimpleFormLocalesRails::Railtie.instance_eval do
+      dirname = Pathname.new(File.dirname(__FILE__)).join(locale_path)
+      I18n.load_path.push dirname
+    end
+  end
+end
