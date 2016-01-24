8749d8d8fb4eb428f3cf6494bbbf28faba43b8b5
Sugano Yoshihisa(E) <koshian@foxking.org>
2016-01-24 19:20:46 +0900

    ('A`)

--- lib/simple_form_locales_rails/railtie.rb
+++ lib/simple_form_locales_rails/railtie.rb
@@ -5,7 +5,7 @@ module SimpleFormLocalesRails
     locale_path = '../../simple_form_locales/locales/*.yml'
     SimpleFormLocalesRails::Railtie.instance_eval do
       files = Dir[File.join(File.dirname(__FILE__), locale_path)]
-      I18n.load_path.push files
+      I18n.load_path.concat files
     end
   end
 end
